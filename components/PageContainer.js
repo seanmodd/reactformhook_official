225051f6248a4281947fb965909a9b30c8dc1548
Sean <sean@vestaridefair.com>
2021-08-20 16:02:32 -0700

    validation initial additions again

--- components/PageContainer.js
+++ components/PageContainer.js
@@ -4,12 +4,7 @@ const PageContainer = ({ children, title }) => {
   const { colorMode } = useColorMode();
 
   return (
-    <VStack
-      minh="100vh"
-      maxh="100%"
-      bg={colorMode === 'light' ? 'gray.100' : 'gray.800'}
-      transition="background 100ms linear"
-    >
+    <VStack>
       <Heading pt={24} as="h1" size="2xl" pb={24} textAlign="center">
         {title}
       </Heading>
